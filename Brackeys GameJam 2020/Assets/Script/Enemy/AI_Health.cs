8c58fc61c2a2cd4e762100d4d0b8cbc34c0d86ad
SuperWizarder <quanganhryan@gmail.com>
2020-08-06 15:44:06 +0700

    Stuff

--- Brackeys GameJam 2020/Assets/Script/Enemy/AI_Health.cs
+++ Brackeys GameJam 2020/Assets/Script/Enemy/AI_Health.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 public class AI_Health : MonoBehaviour
 {
     public float health;
-    public float healthPerSec;
+    public int healthPerSec;
+
+    public float upgradeHealth;
+
+    public GameObject nextLevel;
 
     // Update is called once per frame
     void Update()
     {
         health += healthPerSec * Time.deltaTime;
+
+        if(health >= upgradeHealth)
+        {
+            LevelUp();
+        }
+    }
+
+    void LevelUp()
+    {
+        Destroy(gameObject);
+        Instantiate(nextLevel, transform.position, transform.rotation);
     }
 }
