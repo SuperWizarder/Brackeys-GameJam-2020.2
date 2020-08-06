fe9015ad270f66aec634513db729c8b1f2b9d067
SuperWizarder <quanganhryan@gmail.com>
2020-08-06 15:00:36 +0700

    Removing Bolt

---
+++ Brackeys GameJam 2020/Assets/Script/Enemy/AI_Health.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Health : MonoBehaviour
+{
+    public float health;
+    public float healthPerSec;
+
+    // Update is called once per frame
+    void Update()
+    {
+        health += healthPerSec * Time.deltaTime;
+    }
+}
