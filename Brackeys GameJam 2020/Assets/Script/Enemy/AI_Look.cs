213aa51d31283ee6c5258c908b51790d786c57e9
SuperWizarder <quanganhryan@gmail.com>
2020-08-07 09:12:37 +0700

    Materials

---
+++ Brackeys GameJam 2020/Assets/Script/Enemy/AI_Look.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Look : MonoBehaviour
+{
+	#region Variables
+	public float speed;
+	private GameObject target;
+	#endregion
+
+	#region Unity Methods
+	void Start()
+    {
+		target = GameObject.FindWithTag("Player");
+    }
+	void FixedUpdate()
+    {
+		Vector3 direction = target.transform.position - this.transform.position;
+		direction.y = 0;
+
+		this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), speed);
+
+	}
+	#endregion
+}
---
+++ Brackeys GameJam 2020/Assets/Script/Enemy/AI_Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Move: MonoBehaviour
+{
+	#region Variables
+	private Transform target;
+    public float speed;
+
+    private Rigidbody rb;
+	#endregion
+
+	#region Unity Methods
+	void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 pos = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        rb.MovePosition(pos);
+    }
+	#endregion
+}
