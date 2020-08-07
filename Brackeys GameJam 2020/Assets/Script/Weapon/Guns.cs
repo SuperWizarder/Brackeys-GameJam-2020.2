e1810505fec2c834d9805cf6383177a7294cf38f
SuperWizarder <quanganhryan@gmail.com>
2020-08-08 02:06:09 +0700

    Maps

---
+++ Brackeys GameJam 2020/Assets/Script/Weapon/Guns.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+
+public class Guns : MonoBehaviour
+{
+    public float damage, range, spread;
+    public float amountOfBullets;
+
+    public float shakeMagnitude, shakeRoughness,fadeInTime,fadeOutTime;
+
+    public ParticleSystem muzzleFlash;
+    public GameObject impactFX;
+
+    public Camera fpsCam;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+        CameraShaker.Instance.ShakeOnce(shakeMagnitude, shakeRoughness, fadeInTime, fadeOutTime);
+
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position,fpsCam.transform.forward, out hit, range))
+        {
+            AI_Health health = hit.transform.GetComponent<AI_Health>();
+            if (health != null)
+            {
+                health.TakeDamage(damage);
+            }
+
+            GameObject impactGO = Instantiate(impactFX, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 0.25f);
+        }
+    }
+}
