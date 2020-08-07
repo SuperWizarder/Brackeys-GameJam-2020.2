d122b7674e02529f73c3718c68259a24da62fc82
SuperWizarder <quanganhryan@gmail.com>
2020-08-08 06:24:57 +0700

    PowerUp

---
+++ Brackeys GameJam 2020/Assets/Script/Environment/MovementPowerUp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Configuration;
+using UnityEngine;
+
+public class MovementPowerUp : MonoBehaviour
+{
+    public GameObject pickupEffect;
+
+    public float duration;
+    public float multiplier;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(PickUp(other));
+        } 
+    }
+
+    IEnumerator PickUp(Collider player)
+    {
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        PlayerMovement stats = player.GetComponent<PlayerMovement>();
+        stats.defaultSpeed *= multiplier;
+
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+
+        yield return new WaitForSeconds(duration);
+
+        stats.defaultSpeed /= multiplier;
+
+        Destroy(gameObject);
+    }
+}
