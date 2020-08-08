589a577f928f34d098b1cb2c2a333ad3006fcb75
QuanLam2006 <65981511+QuanLam2006@users.noreply.github.com>
2020-08-07 19:56:06 -0500

    GrapplingGun

---
+++ Brackeys GameJam 2020/Assets/Script/Weapon/GrapplingGun.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class GrapplingGun : MonoBehaviour
+{
+	private LineRenderer lr;
+	private Vector3 grapplePoint;
+	public LayerMask whatIsGrapplable;
+	public Transform gunTip, mainCamera, player;
+	float maxDistance = 100f;
+	private SpringJoint joint;
+
+		private void Awake()
+	{
+		lr = GetComponent<LineRenderer>();
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(1))
+		{
+			StartGrapple();
+		}
+		else if (Input.GetMouseButtonUp(1))
+		{
+			StopGrapple();
+		}
+	}
+
+	private void LateUpdate()
+	{
+		DrawRope();
+	}
+
+	void StartGrapple()
+	{
+		RaycastHit hit;
+		if (Physics.Raycast(mainCamera.position, mainCamera.forward, out hit, maxDistance))
+		{
+			grapplePoint = hit.point;
+			joint = player.gameObject.AddComponent<SpringJoint>();
+			joint.autoConfigureConnectedAnchor = false;
+			joint.connectedAnchor = grapplePoint;
+
+			float distanceFromPoint = Vector3.Distance(player.position, grapplePoint);
+
+			joint.maxDistance = distanceFromPoint * .8f;
+			joint.minDistance = distanceFromPoint * .25f;
+
+			joint.spring = 4.5f;
+			joint.damper = 7f;
+			joint.massScale = 4.5f;
+
+			lr.positionCount = 2;
+		}
+	}
+
+	void DrawRope()
+	{
+		if (!joint) return;
+
+		lr.SetPosition(0, gunTip.position);
+		lr.SetPosition(1, grapplePoint);
+	}
+
+	void StopGrapple()
+	{
+		lr.positionCount = 0;
+		Destroy(joint);
+	}
+}
