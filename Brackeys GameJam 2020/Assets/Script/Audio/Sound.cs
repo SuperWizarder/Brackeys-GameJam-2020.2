1fa0e3dab466292c9cdadd0ac5cf95e95ab16293
QuanLam2006 <65981511+QuanLam2006@users.noreply.github.com>
2020-08-07 20:46:41 -0500

    Audio

---
+++ Brackeys GameJam 2020/Assets/Script/Audio/Sound.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+[Serializable]
+public class Sound
+{
+    #region Variables
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(.1f, 3f)]
+    public float pitch;
+    [Range(0f, 1f)]
+    public float spacialBlend;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+    #endregion
+}
